9ee29e7e0d11e0efd425e94d7fb08062fc3eca05
Jiaming Guo <j3guo@icloud.com>
2022-12-26 21:33:43 -0800

    Initial Commit

---
+++ PostWeibo/PostWeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostWeiboApp.swift
+//  PostWeibo
+//
+//  Created by Jiaming Guo on 2022-12-26.
+//
+
+import SwiftUI
+
+@main
+struct PostWeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
